048278cc8b25a78920726adfa9d8d8cd02e502d4
bosheng <411425403@qq.com>
2017-11-05 18:12:18 +0800

    refactor serializer

--- dataform/serializers.py
+++ dataform/serializers.py
@@ -4,28 +4,33 @@
 from rest_framework import serializers
 from dataform.models import Snippet
 
-class SnippetSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    code = serializers.CharField()
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.CharField(default='python')
-    style = serializers.CharField(default='friendly')
+class SnippetSerializer(serializers.ModelSerializer):
 
-    def create(self, validated_data):
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
-        return Snippet.objects.create(**validated_data)
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
 
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
+    # id = serializers.IntegerField(read_only=True)
+    # title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    # code = serializers.CharField()
+    # linenos = serializers.BooleanField(required=False)
+    # language = serializers.CharField(default='python')
+    # style = serializers.CharField(default='friendly')
+
+    # def create(self, validated_data):
+    #     """
+    #     Create and return a new `Snippet` instance, given the validated data.
+    #     """
+    #     return Snippet.objects.create(**validated_data)
+
+    # def update(self, instance, validated_data):
+    #     """
+    #     Update and return an existing `Snippet` instance, given the validated data.
+    #     """
+    #     instance.title = validated_data.get('title', instance.title)
+    #     instance.code = validated_data.get('code', instance.code)
+    #     instance.linenos = validated_data.get('linenos', instance.linenos)
+    #     instance.language = validated_data.get('language', instance.language)
+    #     instance.style = validated_data.get('style', instance.style)
+    #     instance.save()
+    #     return instance
