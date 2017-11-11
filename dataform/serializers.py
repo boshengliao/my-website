4ab8b107376503520267eb25d10a99953319b802
bosheng <411425403@qq.com>
2017-11-11 22:52:29 +0800

    add TopMenuModelSerializer

--- dataform/serializers.py
+++ dataform/serializers.py
@@ -2,7 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from rest_framework import serializers
-from dataform.models import Snippet
+from dataform.models import (Snippet,
+                             TopMenuModel,
+                             SubMenuModel,
+                             ItemModel)
+
 
 class SnippetSerializer(serializers.ModelSerializer):
 
@@ -10,27 +14,10 @@ class SnippetSerializer(serializers.ModelSerializer):
         model = Snippet
         fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
 
-    # id = serializers.IntegerField(read_only=True)
-    # title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    # code = serializers.CharField()
-    # linenos = serializers.BooleanField(required=False)
-    # language = serializers.CharField(default='python')
-    # style = serializers.CharField(default='friendly')
 
-    # def create(self, validated_data):
-    #     """
-    #     Create and return a new `Snippet` instance, given the validated data.
-    #     """
-    #     return Snippet.objects.create(**validated_data)
+class TopMenuModelSerializer(serializers.ModelSerializer):
 
-    # def update(self, instance, validated_data):
-    #     """
-    #     Update and return an existing `Snippet` instance, given the validated data.
-    #     """
-    #     instance.title = validated_data.get('title', instance.title)
-    #     instance.code = validated_data.get('code', instance.code)
-    #     instance.linenos = validated_data.get('linenos', instance.linenos)
-    #     instance.language = validated_data.get('language', instance.language)
-    #     instance.style = validated_data.get('style', instance.style)
-    #     instance.save()
-    #     return instance
+    class Meta:
+        model = TopMenuModel
+        fields = ('id', 'name', 'description',
+                  'introduction', 'active')
