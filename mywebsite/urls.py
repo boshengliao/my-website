3a8780d3e5971dbb81a83d61f860643ef5db6747
bosheng <411425403@qq.com>
2017-11-11 23:06:19 +0800

    add router about TopMenuViewSet

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from dataform import views
+
+from rest_framework.routers import DefaultRouter
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'topmenu', views.TopMenuViewSet)
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^dataform/', include('dataform.urls')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # url(r'^dataform/', include('dataform.urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include(router.urls)),
 ]
