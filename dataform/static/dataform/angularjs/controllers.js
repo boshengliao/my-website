6cd04cfef8e3cee516b9b504842541a0500893dc
bosheng <411425403@qq.com>
2018-01-07 15:00:47 +0800

    add click event

--- dataform/static/dataform/angularjs/controllers.js
+++ dataform/static/dataform/angularjs/controllers.js
@@ -9,7 +9,23 @@ function test($scope){
     $scope.hi = 'hi...'
 }
 
-function homeCtrl($scope){
+myapp.factory('myApi', myApi)
+function myApi($http){
+    var r = {}
+    r.multiply = function(x, y){
+        return x * y
+    }
+
+    return r
+}
+
+function homeCtrl($scope, myApi){
     log('home')
     $scope.hi = 'hi...'
+    $scope.a = 2
+    $scope.b = 3
+    $scope.result = $scope.a * $scope.b
+    $scope.getResult = function(){
+        $scope.result = myApi.multiply($scope.a, $scope.b)
+    }
 }
